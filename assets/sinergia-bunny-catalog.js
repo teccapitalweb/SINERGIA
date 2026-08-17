5991c131e0eb6b893a52bd37041d598b572f78b0
TEC Capital web <teccapitalweb@gmail.com>
2026-08-17 10:33:57 -0600

    Add files via upload

---
+++ assets/sinergia-bunny-catalog.js
@@ -0,0 +1,351 @@
+// Generado desde sinergia-bunny-upload-state.json. No contiene claves privadas.
+window.SINERGIA_BUNNY_CATALOG = [
+  {
+    "titulo": "ADMINISTRACIÓN Y OPERACIÓN DE VIVEROS",
+    "collectionId": "56dcf419-79b3-4909-9bbf-a396615967ce",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "bd6516b7-05ee-4e19-af3c-69291a707e82"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "946a50fe-4976-4dc8-88c5-a76200d3e691"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "9db7e124-61a0-4daf-91d4-376a77a5b291"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "d2ce7859-8289-4e4b-81e7-8dfb17d1c76a"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "457c4c21-3637-4454-909b-6666e4e6f6a6"
+      }
+    ]
+  },
+  {
+    "titulo": "CERTIFICACIÓN ORGÁNICA",
+    "collectionId": "0c94e8c9-4371-49c7-8b18-89b7b43bd876",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "cdfb6328-cf57-4684-910e-0270a93742ac"
+      },
+      {
+        "numero": 2,
+        "titulo": "SESION 2",
+        "provider": "bunny",
+        "videoId": "17fe3887-7959-4dc7-816a-ea9d6016a167"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "c2ffab10-9ddd-4558-bc13-6562cc8ff411"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "59c584b5-4b8e-4e7c-84ee-247f8451adcd"
+      }
+    ]
+  },
+  {
+    "titulo": "DEL CACAO AL CHOCOLATE",
+    "collectionId": "a89f619e-57df-499e-bbbc-cb8cada44be1",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "279bf5e7-9439-4b85-8572-1101d571866b"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "e555aa72-d053-48bb-8671-600f6fad5378"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "fa927209-e17e-46cf-b007-68dff647c7d1"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "b90e41e7-c9e8-4b76-bab2-6670cf1a2adc"
+      }
+    ]
+  },
+  {
+    "titulo": "ELABORACIÓN DE BIOINSUMOS",
+    "collectionId": "4bfe816a-1a57-47fa-acf6-106d33a19e68",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "8eb50eae-c825-40cc-b991-6df89e6312ae"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "7c6fd06b-0e25-4760-8d7a-5ac47d2eab3a"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "989278b8-7f21-4d70-ba2c-9473fdbf1afb"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "03f6c9d3-7127-4996-827a-6cbf3b9d2426"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "33c3f698-463c-4c6f-873a-1df4abcb7535"
+      }
+    ]
+  },
+  {
+    "titulo": "FERTILIZACIÓN DE ARBOLES FRUTALES",
+    "collectionId": "843d8569-070d-4d21-92dc-2981e287a72f",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "58ba6f97-6d1f-4873-a8a4-9cbf0a4e8f16"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "aece4b09-4125-4d97-8088-db7b118796b0"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "28115105-3cab-4147-a98a-4c8d9e371d06"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "bc824e1c-8fe6-4a93-8c73-d7f27634f247"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "42bf2f41-a4fc-4b78-978d-b61d41b06649"
+      }
+    ]
+  },
+  {
+    "titulo": "FERTIRRIEGO",
+    "collectionId": "6c053e2a-0ef0-4d83-9db8-a4840335e9cd",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "fc0fd3b0-de97-4145-a11a-7a12fa491282"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "111cd901-795b-4aef-915a-6c076245f21a"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "28908ab9-663e-4525-bdf3-25ab435b7f18"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "664df746-d7e4-4731-bd63-c7ed5744126e"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "597cde34-2d2a-426d-acdd-bbf8f00723c8"
+      }
+    ]
+  },
+  {
+    "titulo": "FITOHORMONAS Y ESTIMULADORES DE CRECIMIENTO EN CULTIVOS",
+    "collectionId": "56272ed2-3abc-4f96-8273-ce1a1790b88e",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "fdb8f3e1-2900-4521-8cc5-ac8f5d45fcd7"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "5b9a67be-5f93-446a-9783-aa0a1d9df31e"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "a50e88b5-2115-49dc-8538-179bc7f74311"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "9e07a621-3f3b-4925-8da5-c9ec9acd84ab"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "6d3d8d1e-86ca-4ae1-9eb2-ef8c52fc198d"
+      }
+    ]
+  },
+  {
+    "titulo": "GESTIÓN ESTRATÉGICA DE CLIENTES Y VENTAS AGRO",
+    "collectionId": "ad3f7b5f-f7a4-4fcc-9c73-7f9d8590342b",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "05367511-8bea-4027-9908-be14ca18510b"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "359795c6-d29f-4968-97f4-cb5ea0666091"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "af1df213-d7de-46d4-a7d4-e3a044a16d2b"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "519d090d-642f-4eb8-8492-0c483e0fc285"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "9c4eb664-0851-4086-a3f5-10a3c05cfa50"
+      }
+    ]
+  },
+  {
+    "titulo": "INOCUIDAD ALIMENTARIA Y BUENAS PRÁCTICAS AGROALIMENTARIAS",
+    "collectionId": "89e05a0c-d0ae-4e4a-bb4b-d5b382899f7f",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "6d229b21-896a-4d7d-8d73-31e7bb3e8ab3"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "4a8d8c55-ace6-4238-a070-3aa9281012f0"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "23665544-9519-4069-8a8f-abcb4dc29239"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "b92fd8da-11a0-41cd-b04f-50a2f38bb5e6"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "8e7c721d-c978-4adc-bfd4-48da25b5a6b4"
+      }
+    ]
+  },
+  {
+    "titulo": "LOMBRICOMPOSTA",
+    "collectionId": "9c2c10d2-1037-405f-b6c1-0018a1713a93",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "36ad558e-837d-4616-9a37-1530a9629e5c"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "8ab2750c-8e68-4c8f-a6e4-98fc0bcb0caa"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "669201a6-2930-4961-9666-05c6b2b4fd70"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "1c1315b0-b971-4185-8a8e-4aeccb306b72"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "0895c27d-a6d5-42bb-a088-b8cd715461c9"
+      }
+    ]
+  }
+];
